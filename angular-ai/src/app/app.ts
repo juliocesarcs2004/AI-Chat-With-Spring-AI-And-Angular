4a494a3bcf407aa6be7720c6ca295ab324592c28
juliocesarcs2004 <juliocesarcs2004@gmail.com>
2026-08-07 19:27:14 -0300

    Implementada tela inicial do Chat

--- angular-ai/src/app/app.ts
+++ angular-ai/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatButtonModule, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('angular-ai');
+  protected readonly title = 'AI-Spring-Angular';
 }
