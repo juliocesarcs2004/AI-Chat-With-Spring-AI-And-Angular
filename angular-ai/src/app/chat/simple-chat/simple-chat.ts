5bc4f85ab2019e2ee8b56a133477bf89e6322cda
juliocesarcs2004 <juliocesarcs2004@gmail.com>
2026-08-11 19:26:37 -0300

    Criado o botão Ask Anything

--- angular-ai/src/app/chat/simple-chat/simple-chat.ts
+++ angular-ai/src/app/chat/simple-chat/simple-chat.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-simple-chat',
-  imports: [MatCardModule, MatToolbarModule],
+  imports: [MatCardModule, MatToolbarModule, MatInputModule, MatButtonModule, MatIconModule],
   templateUrl: './simple-chat.html',
   styleUrl: './simple-chat.scss',
 })
 export class SimpleChat {
+
+  messages = signal([
+    { text: 'Hello! How can I help you today ?', isBot: true }
+  ])
 
 }
